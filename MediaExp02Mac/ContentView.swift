65f0f91fdf63470c37b43cee1ae85697306929a8
meqt <meqt@foxmail.com>
2023-05-28 21:16:11 +0800

    Initial Commit

---
+++ MediaExp02Mac/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MediaExp02Mac
+//
+//  Created by 蒋艺 on 2023/5/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediaExp02Mac/MediaExp02MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaExp02MacApp.swift
+//  MediaExp02Mac
+//
+//  Created by 蒋艺 on 2023/5/28.
+//
+
+import SwiftUI
+
+@main
+struct MediaExp02MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
